1c30cadd773962388495bd30e4ef5a6cefed9491
Ari <akmadian@gmail.com>
2020-04-23 17:09:43 -0700

    Add some more content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useRef, useEffect } from 'react';
+import { Row, Col, Layout } from 'antd';
 import { Bar } from '@antv/g2plot';
+import YouTube from 'react-youtube';
+
+const { Header, Footer, Content } = Layout;
 
 const App = () => {
   const data = [
@@ -31,11 +35,60 @@ const App = () => {
 
   return (
     <div className="App">
-      <header>
-        <h1>Physics 223 Project</h1>
-        <h2>whatever it is</h2>
-      </header>
-      <div ref={container}></div>
+      <Row id="content">
+        <Col span={10} offset={7}>
+          <header>
+            <h1>The Doppler Effect</h1>
+          </header>
+          <div id="abstract">
+            <p>
+              The Doppler Effect refers to the change in frequency of a wave
+              from the point of view of an observer, who is moving relative to
+              the wave source.
+            </p>
+            <p>
+              This effect can be seen (and heard) every day, and is used for 
+              all kinds of cool things inlcuding robotics, astronomy, radar 
+              technology, medical technology, flow measurement, biology,
+              satellite communication, and audio technology!
+            </p>
+            <YouTube videoId="a3RfULw7aAY"/>
+          </div>
+          <div id="group">
+            <h2>Group Members and Tasks</h2>
+            <Row>
+              <Col span={6} offset={3}>
+                Iyari Angeles
+              </Col>
+              <Col span={6}>
+                Dylan Pool
+              </Col>
+              <Col span={6}>
+                Ari Madian
+              </Col>
+            </Row>
+          </div>
+          <div>
+            REFERENCES
+            <p id="ref-1">
+              [<a href="#uref-1">i</a>]&nbsp;
+              <a href="https://en.wikipedia.org/wiki/Doppler_effect">
+                Wikipedia.com "Doppler Effect"
+              </a>
+            </p>
+            <p id="ref-2">
+              [<a href="#uref-2">ii</a>]&nbsp;
+              <a href="https://www.physicsclassroom.com/class/waves/Lesson-3/The-Doppler-Effect">
+                physicsclassrom.com Vibrationa and Waves - Lesson 3 - Behavior of Waves - The Doppler Effect
+              </a>
+            </p>
+          </div>
+        </Col>
+      </Row>
+      <footer>
+        Website created by Ari Madian for PHYS&223 - Spring 2020 @ South Seattle College. <br/>
+        <a href="https://github.com/akmadian/phys223-project">This site's source code</a> is MIT licensed.
+      </footer>
     </div>
   );
 }
